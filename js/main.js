d660845ced868442c3886ca45c657e5aed0529f8
elias alvarez <ealvarez@cloud.studio>
2019-03-31 03:46:56 -0300

    Se sube smoth scroll para nav, se agrega limpieza de formulario

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
+        
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".gridMenuTop", offset: 50});   
+
+    // Add smooth scrolling on all links inside the navbar
+    $("#navMenuTop a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
     
-	$('#navMenuTop').localScroll({duration:800});
-	
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        }  // End if
+    });
+    
     $('#alert').hide();
     
     $("#formulario").on("submit", function(e) {
@@ -11,16 +34,13 @@ $(document).ready(function() {
         
         e.preventDefault();
         
-        var theBtn = $('#btnEnviar');
+        var theBtn = $('#btnEnviar');                
+        //Codigo para desabilitar boton de envio.
+        
+        theBtn.addClass('disabled');
 
-        if ( $('#btnEnviar').is('.disabled')) {
-            theBtn.removeClass('disabled'); // enable the button?
-            lightFill(); //show fill inputs
-            lightEmpty(); //show empty inputs
-            return false;
-        }
+        var data = $("#formulario").serialize();
 
-            var data = $("#formulario").serialize();
         $.ajax({
             url: 'http://sergiomassa.com.ar/api/mail.php',
             type: 'POST',
@@ -29,8 +49,11 @@ $(document).ready(function() {
             //some code going here if success 
             if(data.message) {
                 $('#responseMessage').text(data.message);
+                //Reseteamos formulario
+                form.closest("form").trigger("reset");       
                 $('#alert').show(); 
-            }
+            } 
+            theBtn.removeClass('disabled'); 
         }).fail(function() {
             if(data.message) {
                 $('#responseMessage').text(data.message);
